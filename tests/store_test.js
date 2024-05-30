7d4de16ed8d3c40209ce9d3870ef26776e8b55d9
Alex <60351618+Dev4Lex@users.noreply.github.com>
2024-05-30 20:14:41 +0300

    first scenario

--- tests/store_test.js
+++ tests/store_test.js
@@ -1,5 +1,19 @@
+const { pause } = require("codeceptjs");
+
 Feature('store');
 
-Scenario('test something',  ({ I }) => {
-
+Scenario('test something', ({ I }) => {
+    I.amOnPage('http://opencart.qatestlab.net/');
+    I.click({ xpath: '//*[@id="top-links"]/ul/li/span/span' });
+    I.click({ xpath: '//a[.="Register"]' });
+    I.see('Register Account');
+    I.fillField({css: '#input-firstname'}, 'Alex');
+    //pause();
 });
+
+Scenario('grab smth', async ({ I }) => {
+I.amOnPage('http://opencart.qatestlab.net/index.php?route=product/product&product_id=29');
+I.waitForVisible({css: 'h2.product-title'}, 5);
+let price = await I.grabTextFrom('(//span[@class="price-new"])[1]');
+console.log(price);
+}).tag('grab');
