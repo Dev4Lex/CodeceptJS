ae96c21c5b11f0c0f23630de2dd42dc00886c32d
Alex <60351618+Dev4Lex@users.noreply.github.com>
2024-06-04 01:41:56 +0300

    update buyProduct test

--- tests/buyProduct_test.js
+++ tests/buyProduct_test.js
@@ -5,8 +5,10 @@ let loginUser = {
 
 Feature('buy');
 
-Scenario('buy products', ({ I, homePage, registerPage }) => {
+Scenario('buy products', async ({ I, productPage }) => {
     I.login(loginUser);
     I.amOnPage('http://opencart.qatestlab.net/index.php?route=product/product&path=31&product_id=40');
+    let price = await productPage.getProductPrice();
+    console.log(price);
     pause();
 }).tag('buy');
