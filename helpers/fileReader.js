985b76672a071df6cdcb951574a436b6412d59eb
Alex <60351618+Dev4Lex@users.noreply.github.com>
2024-06-07 03:09:48 +0300

    add fileReader

---
+++ helpers/fileReader.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const { link } = require('fs/promises');
+const FILE_PATH = './input/links.txt';
+
+module.exports = {
+    readContectFromFile() {
+        try {
+            return fs.readFileSync(FILE_PATH, 'utf8');
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },
+
+    getArrayFromString(string){
+        return this.readContectFromFile().split('\n');
+    },
+
+    getArrayOfProductLinkObjects(array) {
+        let arrayOfObjects = [];
+        for (const element of array) {
+            arrayOfObjects.push({ link: element });
+        }
+        return arrayOfObjects;
+    }
+}
