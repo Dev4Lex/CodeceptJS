4e409fb938b34cd7a8ddba5703fc315896712e4e
Alex <60351618+Dev4Lex@users.noreply.github.com>
2024-06-11 02:44:32 +0300

    fix converter

--- helpers/converter_helper.js
+++ helpers/converter_helper.js
@@ -2,7 +2,7 @@ const Helper = require('@codeceptjs/helper');
 
 class Converter extends Helper {
 
-  parsePrice(string){
+  async parsePrice(string){
     return parseFloat(string.replaceAll(/[^0-9\.]+/g, ''));
   }
 
